1d1d90510b325ea55dd8c1014c41aa72fcbae0cf
FinnLEA <56691691+FinnLEA@users.noreply.github.com>
2021-03-17 20:43:42 +0500

    fix lab4

--- main.cpp
+++ main.cpp
@@ -59,16 +59,13 @@ int main(int argc, char** argv, char** envp){
         }
     }
 
+    int status = 0;
     if(subPid != 0){
-        pid_t p;
-        pid_t parentPid = getppid();
-        int status;
-        p = waitpid(-1, &status, 0);    
-        while (p != -1){
-            p = waitpid(-1, &status, 0);
-        }
-        fclose(handle);
-        exit(0);
+        while (wait(&status) > 0) {
+		    if (status != 0) {
+				exit(1);
+			}
+		}
     }
     
     fclose(handle);
